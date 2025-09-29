3c3a2033a3ff6b33be83f21a4183507fd63eee75
jakejacobs2015 <jacky4uonly2009@gmail.com>
2025-09-29 15:35:16 +0530

    initial commit. Introductory class.

---
+++ demo.py
@@ -0,0 +1,46 @@
+#python programming language
+
+#language = communication with other humans
+# examples: Telugu, English...
+
+#programming language = communication between humans 
+#                       and computers
+# examples: C, Python, Javascript, Java
+
+# Python
+# Data science, Machine learning, Web developement
+# Easy to use, easy to learn
+
+# English : I am a student
+#            I student am a    
+
+# German: Guten morgen, Hallo
+# English words: hello, hi, mornign, computer
+# strings: 'hello', 'hi', 'morning', 'computer'
+            #'welcome'
+
+
+# numbers: 1,2 34, 4.6
+# integers: 1,2,3,10, 100
+# floats: 1.4, 3.14, 99999.9999
+
+
+# Basic data types
+# 1. strings # 'hi'
+# 2. integers # 2
+# 3. floats # 3.14
+
+#Human to Human
+# Writing, Speaking, Gestures
+
+# Human to computer
+# Coding --> python interpreter --> computer
+# 'hi' ---> 0101010101 
+
+# You don't need internet to run python in a computer.
+
+# print('hello')
+# print(23)
+# print(3.14)
+
+# print('hello there')
---
+++ p1.py
@@ -0,0 +1,6 @@
+print(2)
+print(3.14)
+print('hi')
+
+print("it's me. I'm from here.")
+print()
