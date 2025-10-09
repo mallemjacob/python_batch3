f1b45b5af7e99979071fe0a3db9796e586e851f2
jakejacobs2015 <jacky4uonly2009@gmail.com>
2025-10-09 17:14:04 +0530

    Add if statements

---
+++ if-statement.py
@@ -0,0 +1,38 @@
+# This is my program
+# Writte by jakey
+# on octiber 9th
+# on the topic if staments.
+
+# print('Enter your name:')
+# name = input() #julia
+# print('hi ' + name) # hi julia
+# name_len = len(name) # 5
+# stringy_name_len = str(name_len) # '5'
+# print('Your name length is ' + stringy_name_len )
+
+# print('enter your password:')
+# password = input() #fish
+# password_length = len(password) #4
+
+# if password_length <= 8:
+#   print('The password must be greater than 8 charcaters.')
+# else:
+#   print('Good password')
+
+
+# =  --> assignment statement
+# == --> equality operator  
+
+# age = int(input('Enter your age: '))
+# if  age <= 10 : 
+#   print('You need your parents')
+# elif age > 10 and age < 20:
+#   print('you can drive a bicycle')
+# elif age >= 20 and age <= 50:
+#   print('You can drive a car')
+# else:
+#   print('you can stay at home.')
+
+# print('The end')
+
+# https://www.online-python.com/
