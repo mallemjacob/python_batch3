4b54d24670779c9b0796805ccd1ea02e7a83a875
jakejacobs2015 <jacky4uonly2009@gmail.com>
2025-10-11 17:11:28 +0530

    Add password_counter to the while loop.

--- while.py
+++ while.py
@@ -12,10 +12,10 @@
 #     print(i)
 
 
-#break 
+# break 
 # while True:
 #     print('Enter name:')
-#     name = input() #julia
+#     name = input() #bill
 #     if name == 'julia':
 #         print('Hi julia')
 #         break
@@ -48,6 +48,13 @@
 #     else:
 #         print(i)
 
+# skip mutiples of 3
+# for i in range(10):
+#     if i % 3 == 0: 
+#         continue
+#     else:
+#         print(i)
+
 # skip number 37
 # for i in range(1, 101):
 #     if i == 37: 
@@ -59,7 +66,7 @@
 # for i in range(10, 0, -1):
 #     print(i)
 
-# print every 3rd numbers
+#print every 3rd numbers
 # for i in range(0, 10, 3): # 0 1 2 3 4 5 6 | 0 2 4 6 8
 #     if i == 6:
 #         break
@@ -68,7 +75,7 @@
 
 # stop the infinite loop with ctrl + c
 
-# using for loop in a while loop
+# using for-loop in a while loop
 # while True:
 #     for i in range(10): # 0 1 2 3 4 5 6 7 8 9
 #         if i == 4:
@@ -94,6 +101,7 @@
 #         print(i)
 #         i = i + 1     
 
+password_counter = 1
 while True:
     print('Enter name:')
     name = input() #bill
@@ -106,8 +114,30 @@ while True:
                 print('Welcome to your account!')
                 break
             else:
-                continue
+                if password_counter != 5:
+                    print('Please enter correct password!')
+                    password_counter += 1
+                    continue
+                else:
+                    print('You have used all of your chances.')
+                    break
+                    
     else:
         print('Enter correct username')
         continue
     break
+
+# break 
+# counter = 3
+# for i in range(counter):
+#     print('Enter name:')
+#     name = input() #bill
+#     if name == 'julia':
+#         print('Hi julia')
+#         break   
+#     else:
+#         if i == counter - 1:
+#             print('You have completed your chances!')
+#         else:
+#             print('not julia')
+#             print('You have only ' + str(counter - (i + 1)) + ' chances left.')
