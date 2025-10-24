27c6b38c33436054eb7138c669f5edd6bce06078
jakejacobs2015 <jacky4uonly2009@gmail.com>
2025-10-24 12:31:35 +0530

    Add functions.py file and covered functions defnitions and return statement.

---
+++ functions.py
@@ -0,0 +1,29 @@
+# Functions
+
+# def greet(name):
+#     print('Welcome ' + name)  
+
+# greet('julia')
+
+# user defined functions
+# function definition
+def add(num1, num2, num3): #parameters
+    #function body
+    print('hi')
+    return num1 + num2 + num3
+
+n1 = int(input('Enter number1: ')) #'2' -- 2
+n2 = int(input('Enter number2: '))
+n3 = int(input('Enter number3: '))
+
+#function calling
+print(add(n1, n2, n3)) # arguments
+
+
+#Built-in functions
+#print()
+#len()
+#input()
+#int()
+#float()
+#str()
