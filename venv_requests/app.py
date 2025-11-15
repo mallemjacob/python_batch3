0358bc0421e769f16371253c8527d4b2d7e49381
jakejacobs2015 <jacky4uonly2009@gmail.com>
2025-11-15 10:57:07 +0530

    Update venv_requests/app.py file. Add new apis

--- venv_requests/app.py
+++ venv_requests/app.py
@@ -1,7 +1,15 @@
-import requests
+import requests, pprint
 
-response = requests.get('https://jsonplaceholder.typicode.com/todos/')
+# response = requests.get('https://jsonplaceholder.typicode.com/todos/')
 
+weatherapi = 'https://api.open-meteo.com/v1/forecast?latitude=16.3067&longitude=80.4365&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m'
+
+
+response = requests.get(weatherapi)
+
+# convert json data into dictionary
 output = response.json()
 
-print(output)
+# pprint.pprint(output[-1]['title'])
+# print(len(output))
+pprint.pprint(output)
