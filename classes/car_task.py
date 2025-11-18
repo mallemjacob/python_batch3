c92676c327154e4799b9cc4b8ab089f3b19f06a5
jakejacobs2015 <jacky4uonly2009@gmail.com>
2025-11-18 13:50:34 +0530

    Add car_task.py

---
+++ classes/car_task.py
@@ -0,0 +1,10 @@
+# Make a Car class
+# Attributes =  make, model, year
+# Methods = get_descriptive_name() -> it return 3 attributes
+# Setting a Default Value for an Attribute:
+#    odometer_reading = 0
+# Write new method, read_odometer()
+# Modifying Attribute Values:
+#   1.Modifying an Attribute’s Value Directly
+#   2.Modifying an Attribute’s Value Through a Method, update_odometer()
+# Reject the change if milage is less than odometer_reading.
