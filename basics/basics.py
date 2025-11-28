b9791eadb250c89d46400aee8529603528067b51
jakejacobs2015 <jacky4uonly2009@gmail.com>
2025-11-28 12:00:53 +0530

    Add basics folder and completed until comparision operators.

---
+++ basics/basics.py
@@ -0,0 +1,111 @@
+'''Ctrl + / to comment multiple selected lines at onces.'''
+# 2 + 2 #4
+# print(2 + 2) #Addtion
+# print(2 - 2) #Subtraciton
+# print(2 * 2) #Multiplication
+# print(2 / 2) #Division
+# print(2 // 2) #Integer divison
+# print(2 ** 3) # 2 * 2 * 2 Exponent
+# print(2 % 2) #Modulus
+
+# Select same items with Ctrl + d and keep pressing until you select all the items.
+# print(2.5 + 2.5) #Addtion
+# print(2.5 - 2.5) #Subtraciton
+# print(2.5 * 2.5) #Multiplication
+# print(2.5 / 2.5) #Division
+# print(2.5 // 2.5) #Integer divison
+# print(2.5 ** 3) # 2.5 * 2.5 * 2.5 Exponent
+# print(2.5 % 2.5) # Modulus
+
+# BODMAS rule
+# print(2 + (3 - 4) + 6 / 2.34)
+
+# Open terminal shortcut
+# ctrl + `
+
+
+# String Concatenation
+# 'hi' 
+# 'there'
+# print('hi ' + 'there')
+# print("welcome" + ' to ' + '''my class''' + """.""")
+# print("*")
+
+# # String Replication
+# print('hi ' * 5)
+# print(('hi  ' + 'there ') * 3)
+
+# print(str(2)) #2
+# print(int('2')) #2
+# print(int(2.4)) #2
+
+# print('I am' + str(20) + ' years old.')
+# print('I am' + '20' + ' years old.')
+
+#'I am 20 years old.'
+
+# Variables
+# Should not start with numbers
+# Should not contain space
+
+# age20 = 20
+# length = 20
+# height = 5.7
+# distance = 100
+# name = 'valkyrie'
+# first_name = '' #snake case
+# my_childhood_house_address = '2/42/16'
+# firstName = ''   #Camel case
+
+# print('The length of scale is ' + str(length) + ' cms')
+
+
+# print('My name is ' + name + ' and i am ' + str(age20) + ' years old. ' + 'My height is ' + str(height) + '. Everyday i travel ' + str(distance) + 'kms. ' + 'I will be ' + str(age20 + 1) + ' by next year.')
+
+
+# print(f"my name is {name}. My age is {age20}")
+
+
+'''Comparison Operators
+-----------------------'''
+'''
+greater than >
+greater than or equal >=
+less than <
+less than <=
+equal to ==
+not equal to !=
+'''
+
+print(2 > 2)
+print(2 < 2)
+print(2 <= 2)
+print(4 < 2)
+print(12 > 2)
+print(4 == 3)
+print(4 == 4)
+print(4 != 4)
+print(4 != 3)
+
+print('mouse' == 'mouse')
+print('mouse' != 'mouse')
+print('mouse' == 'dog')
+print('mouse' != 'dog')
+
+print('****')
+# python is case sensitive
+print('mouse' == 'Mouse')
+print('***' == '***')
+print('***' == '****')
+print('***' != '****')
+
+
+print((2 + 2) > 3) #True
+
+# print('Enter your name:')
+# username = input() # username = mouse
+# if 'your name' != username: # 'your name' != 'mouse':
+#     print(f"hi {username}")
+# else:
+#     # 'your name' != 'your name'
+#     print("Try again")
---
+++ basics/readme.md
@@ -0,0 +1,140 @@
+Language --> Communicate --> Humans to Humans
+
+Telugu, English, Hindi
+
+Programming Language -- > Humans to Computers
+
+Python, Javascript, C
+
+Python
+------
+1. Popular
+2. AI, Machine Learning, Deep learning, Data science
+3. Web developement
+4. DevOps
+5. Automation
+6. Easy
+
+Python Syntax
+--------------
+# data types
+-------------
+# integers
+1,2,3,4...
+
+# floats
+1.34, 2.24, 9.99...
+
+# strings
+'good morning', "bye", 'mouse', '''hi''', """welcome"""
+"hi it's me"
+
+# Arithmetic Operators
++, -, *, **, /, %, //
+
+# Additon 
+2 + 2 --> 4
+# Subtarction
+2 - 2
+# Multiplication
+4 * 3
+# Divison
+10 / 5 = 2.0
+# Integer division
+10 // 5 = 2
+# Exponent
+2 ** 3 --> 2 * 2 * 2
+# Modulus
+10 % 5 = 0
+
+# Strings Operators
++, *
+
+# Concatenation
+'Good ' + 'Morning' -> 'Good Morning'
+
+# Replication
+'Hi' * 3 ---> 'HiHiHi'
+
+
+# Functions
+print() # Show the result on the screen.
+str() # Convert integers and floats into strings.
+int() # Convert strings and floats into integers.
+float() # Convert integers and strings into floats.
+
+# Variables
+# Give lables to values.
+# Store values in variables.
+
+
+#Datatypes
+-----------
+interges = 1,2,3
+floats = 2.5, 3.4
+strings = 'hi', "welcome", '''bye'''
+
+Operators
+---------
+arithmetic = + - / * ** %
+string = + *
+comparison = >, >=, <, <=, ==, !=
+
+Functions
+---------
+print(), str(), int(), float()
+
+Variables
+---------
+name, age, sex, location, color
+first_name, temperature_in_december_2017 = 23
+
+Variables Rules
+---------------
+1. No spaces in variables
+2. Should not start with a number
+3. Should not use special characters except underscore
+
+
+# Conditions
+if you get marks above 90 percent, i will buy you a new laptop or else
+i will punish you.
+
+if condition:
+    code
+else:
+    code
+
+# Booleans
+True, False
+
+Boolean Operators
+------------------
+and, or, not
+
+'hi' + 'there'
+True and True
+True or False
+
+NOT
+not True --> False
+not False --> True
+
+not not not not True --> True
+
+AND
+True and True --> True
+True and False --> False
+False and True --> False
+False and False --> False
+
+OR
+True or True --> True
+True or False --> True
+False or True --> True
+False or False --> False
+
+# Loops
+-------
+1. for loop --> Certain number of times
+2. while loop --> Until it becomes False
